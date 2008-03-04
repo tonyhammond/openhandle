f2a2a2c8e2fc1211758823c07caf243f6225062a
christopher.townson <christopher.townson@bd0e26b3-3447-0410-9544-9387dd2f01e4>
2008-03-04 17:22:37 +0000

    syntax corrections to json template

--- trunk/src/test/java/net/handle/servlet/Sandbox.java
+++ trunk/src/test/java/net/handle/servlet/Sandbox.java
@@ -1,15 +1,9 @@
 package net.handle.servlet;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import net.handle.hdllib.Util;
-
-import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.XMLConfiguration;
 
 public class Sandbox {
 
@@ -43,77 +37,6 @@ public class Sandbox {
                 }
             }
         }
-
-//        String[] indices = new String[] {"0", "1", "2", "", null, "4", "crap"};
-//        int[] indicesConverted = Sandbox.getIndices(indices);
-//        System.out.println("-------------------");
-//        for (int i : indicesConverted) {
-//            System.out.println(i);
-//        }
-//        System.out.println("-------------------");
-//
-//        String[] types = new String[] {"HS_ADMIN", "HS_SITE", "HS_NA_DELEGATE", "HS_SERV", "", "HS_PRIMARY", "HS_VLIST"};
-//        byte[][] encoded = encode(types);
-//        for (byte[] a : encoded) {
-//            for (byte b : a) {
-//                System.out.println(b);
-//            }
-//        }
-//        System.out.println("--------");
-//        byte[][] enumEncoded = HandleRecordType.encode(types);
-//        for (byte[] a : enumEncoded) {
-//            for (byte b : a) {
-//                System.out.println(b);
-//            }
-//        }
-//        System.out.println("--------");
-//        System.out.println(encoded.length == enumEncoded.length);
-//        for (int i = 0; i < encoded.length; i++) {
-//            System.out.println(Arrays.equals(encoded[i], enumEncoded[i]));
-//        }
     }
-
-//    private static int[] getIndices(String[] indices) {
-//        if (indices != null && indices.length > 0) {
-//            Integer[] indicesTransitional = new Integer[indices.length];
-//            for (int i = 0; i < indices.length; i++) {
-//                try {
-//                    indicesTransitional[i] = new Integer(indices[i]);
-//                } catch (NumberFormatException e) {
-//                    System.out.println(e.getMessage());
-//                }
-//            }
-//
-//            int i = 0;
-//            for (Integer integer : indicesTransitional) {
-//                if (integer != null) {
-//                    i++;
-//                }
-//            }
-//
-//            int[] indicesConverted = new int[i];
-//            i = 0;
-//            for (Integer integer : indicesTransitional) {
-//                if (integer != null) {
-//                    indicesConverted[i] = integer;
-//                    i++;
-//                }
-//            }
-//
-//            return indicesConverted;
-//        }
-//
-//        return null;
-//    }
-//
-//    private static byte[][] encode(String[] types) {
-//        byte[][] encoded = new byte[types.length][];
-//        for (int i = 0; i < types.length; i++) {
-//            byte[] type = Util.encodeString(types[i]);
-//            encoded[i] = type;
-//        }
-//
-//        return encoded;
-//    }
 
 }
